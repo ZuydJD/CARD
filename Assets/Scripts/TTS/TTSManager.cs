ae4ef7fe144eadc8928a0cc535aeea81efb5746a
ZuydJD <2106209dang@zuyd.nl>
2023-01-23 15:29:58 +0100

    C.A.R.D. V1.0
    + Prefab variants of original avatar with with same names as reference images
    + Added reference images for image recognition
    + Fixed issue not hearing TTS

--- Assets/Scripts/TTS/TTSManager.cs
+++ Assets/Scripts/TTS/TTSManager.cs
@@ -8,41 +8,41 @@ public class TTSManager : Singleton<TTSManager>
 {
     public SpeechConfig config;
     public SpeechSynthesizer synthesizer;
-    private const int AMPLITUDE_MULTIPLIER = 10;
+    //private const int AMPLITUDE_MULTIPLIER = 10;
     public override void Awake()
     {
         base.Awake();
         config = SpeechConfig.FromSubscription("8156745855a94d0294fe9da21bf5ba9d", "westeurope");
 
         config.SpeechSynthesisVoiceName = "nl-NL-MaartenNeural";
-        synthesizer = new SpeechSynthesizer(config, null);
+        synthesizer = new SpeechSynthesizer(config);
     }
 
     public async Task synthesizeAudioAsync(string text)
     {
         var result = await synthesizer.SpeakTextAsync(text);
-        float amp = GetAmplitude(result.AudioData);
+        //float amp = GetAmplitude(result.AudioData);
         
         //SetBlendShapeWeights(amp);
         //using var stream = AudioDataStream.FromResult(result);
 
     }
 
-    private float GetAmplitude(byte[] audioBytes)
-    {
-            var amplitude = 0f;
+    //private float GetAmplitude(byte[] audioBytes)
+    //{
+            //var amplitude = 0f;
             
 
-            foreach (var sample in audioBytes)
-            {
-                amplitude += Mathf.Abs(sample);
-            }
+            //foreach (var sample in audioBytes)
+            //{
+                //amplitude += Mathf.Abs(sample);
+            //}
 
-            return Mathf.Clamp01(amplitude / audioBytes.Length * AMPLITUDE_MULTIPLIER);
+            //return Mathf.Clamp01(amplitude / audioBytes.Length * AMPLITUDE_MULTIPLIER);
         
 
-        return 0;
-    }
+        //return 0;
+    //}
 
     // Start is called before the first frame update
     void Start()
